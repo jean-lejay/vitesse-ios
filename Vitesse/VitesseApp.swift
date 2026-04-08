6af9380ffaaa1fb8ffa47384a4566dc4da7f3951
Jean Lejay <github.private093@passmail.net>
2026-04-08 14:10:42 +0200

    Initial commit - Vitesse iOS

---
+++ Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Jean Lejay on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
